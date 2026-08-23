73ff9049c17d1df04e5847a0a2cf3344c678efe5
lambdacode.dev <lambdacode.dev@gmail.com>
2026-08-22 21:17:00 -0400

    spsc.cpp now converted into a template class

--- spsc.cpp
+++ spsc.cpp
@@ -10,52 +10,58 @@
 
 constexpr int N = 100; // buffer size: actually only holds N-1 items
 constexpr int M = 10000; // total items
-using Item = int;
-std::array<Item, N> buffer; // current items at [readidx, writeidx)
-alignas(std::hardware_destructive_interference_size) std::atomic<int> readidx{}, writeidx {};
+
+template<typename T>
+class SPSC {
+    std::array<T, N> buffer; // current items at [readidx, writeidx)
+    alignas(std::hardware_destructive_interference_size) std::atomic<int> readidx{}, writeidx {};
+
+    bool full(int r, int w) {
+        return (w+1) % N == r;
+    }
+
+    bool empty(int r, int w) {
+        return w == r;
+    }
+
+public:
+    void write(T item) {
+        int w = writeidx.load(std::memory_order_relaxed), r;
+        do {
+            r = readidx.load(std::memory_order_acquire);
+        }
+        while(full(r, w));
+
+        buffer[w] = item;
+        writeidx.store((w+1) % N, std::memory_order_release);
+    }
+
+    int read() {
+        int w, r = readidx.load(std::memory_order_relaxed);
+        do {
+            w = writeidx.load(std::memory_order_acquire);
+        }
+        while(empty(r,w));
+
+        int item = buffer[r];
+        readidx.store((r+1) % N, std::memory_order_release);
+        return item;
+    }
+};
+
+SPSC<int> queue;
 std::atomic<int> sum {}; 
-
-inline bool full(int r, int w) {
-    return (w+1) % N == r;
-}
-
-inline bool empty(int r, int w) {
-    return w == r;
-}
-
-inline void write(Item item) {
-    int w = writeidx.load(std::memory_order_relaxed), r;
-    do {
-        r = readidx.load(std::memory_order_acquire);
-    }
-    while(full(r, w));
-
-    buffer[w] = item;
-    writeidx.store((w+1) % N, std::memory_order_release);
-}
-
-inline int read() {
-    int w, r = readidx.load(std::memory_order_relaxed);
-    do {
-        w = writeidx.load(std::memory_order_acquire);
-    }
-    while(empty(r,w));
-
-    int item = buffer[r];
-    readidx.store((r+1) % N, std::memory_order_release);
-    return item;
-}
 
 void produce() {
     for(int i = 1; i <= M; ++i) {
-        write(i);
+        queue.write(i);
     }
 }
 
 void consume() {
     int s= 0;
     for(int i = 0; i < M; ++i) {
-        s+= read();
+        s+= queue.read();
     }
     sum.store(s, std::memory_order_release);
 }
