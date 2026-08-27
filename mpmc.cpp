33db35619aa736ace14cb76764b95dea4fdf3d6c
lambdacode.dev <lambdacode.dev@gmail.com>
2026-08-27 09:11:49 -0400

    add mpmc

---
+++ mpmc.cpp
@@ -0,0 +1,84 @@
+#include <array>
+#include <atomic>
+#include <iostream>
+#include <new>
+#include <thread>
+#include <vector>
+
+/*
+ * The main idea is to turn MPMS into effectively a SPSC, by generating monotonically
+ * a read / write slot for an asking reader/writer, and then reader/writer has exlusive
+ * access to the assign slot.
+ */
+template<typename T, int N>
+class MPMC {
+    static_assert( (N & (N-1)) == 0, "Ring buffer size N has to be power of 2");
+
+    // let slot index i increase monotonically, and
+    // use i & mask for actual index into the ring buffer
+    constexpr static int mask = N - 1;
+    std::array<T,N> buffer;
+
+    // assign each slot i with a seq number s.
+    // i == s+1 indicates full / ready for reaad.
+    // i == s indicates empty / ready for write.
+    std::array<std::atomic<int>,N> sequence;
+
+    alignas(std::hardware_destructive_interference_size) std::atomic<int> writeidx {};
+    alignas(std::hardware_destructive_interference_size) std::atomic<int> readidx {};
+
+public:
+    MPMC() {
+        for(int i = 0; i < N; ++i) {
+            // initialize to empty state / ready for writing for each slot
+            sequence[i].store(i, std::memory_order_release);
+        }
+    }
+    void push(T&& item) {
+        auto w = writeidx.fetch_add(1, std::memory_order_acq_rel);
+        auto i = w & mask;
+        while( sequence[i].load(std::memory_order_acquire) != w ) ;
+        buffer[i] = std::move(item);
+        sequence[i].store(w+1, std::memory_order_release);
+    }
+    T pop() {
+        auto r = readidx.fetch_add(1, std::memory_order_acq_rel);
+        auto i = r & mask;
+        while( sequence[i].load(std::memory_order_acquire) != r+1 ) ;
+        T item = std::move(buffer[i]);
+
+        // mark this ring buffer slot i as ready to write by
+        // next write who grab montonical slot r+N
+        // note (r+N) & mask = i
+        sequence[i].store(r+N, std::memory_order_release);
+        return item;
+    }
+};
+
+
+int main() {
+    MPMC<int, 8> que;
+    constexpr int M = 128;
+    alignas(std::hardware_destructive_interference_size) std::atomic<int> sum {};
+    {
+        std::vector<std::jthread> producers;
+        for(int i = 0; i < M; ++i) {
+            producers.push_back( std::jthread([&que] {
+                        for(int j = 1; j <= M; ++j) {
+                        que.push(std::move(j));
+                        }
+                        }));
+        }
+        std::vector<std::jthread> consumers;
+        for(int i = 0; i < M; ++i) {
+            consumers.push_back( std::jthread([&que, &sum] {
+                        for(int j = 1; j <= M; ++j) {
+                        auto val = que.pop();
+                        sum.fetch_add(val, std::memory_order_acq_rel);
+                        }
+                        }));
+        }
+    }
+    std::cout << "sum " << sum << "\n";
+    return 0;
+}
